e40bfa4b7a574b6b923a2b6b9f5f820812c88cd8
Pete Schwamb <pete@schwamb.net>
2022-05-10 12:20:43 -0500

    Reproduce navigation bar hiding issue

--- ModalTest2/ViewController.swift
+++ ModalTest2/ViewController.swift
@@ -6,14 +6,81 @@
 //
 
 import UIKit
+import SwiftUI
 
-class ViewController: UIViewController {
+// MARK: Normal Loop presenting device vc
+
+//class MainViewController: UINavigationController {
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//    }
+//
+//    override func viewDidAppear(_ animated: Bool) {
+//        let modal = DeviceManagerVC()
+//        present(modal, animated: true)
+//    }
+//}
+
+
+// MARK: Onboarding presenting device vc
+
+struct MainOnboardingFlow: View {
+    @State private var presented: Bool = false
+    var topLevelNavigationView: Bool
+
+    var body: some View {
+        if topLevelNavigationView {
+            // .navigationBarHidden(true) is *not* respected in this case on iOS 15
+            NavigationView {
+                content
+            }
+        } else {
+            // .navigationBarHidden(true) is respected in this case
+            content
+        }
+    }
+
+    var content: some View {
+        VStack {
+            Text("Onboarding")
+                .sheet(isPresented: $presented) { DeviceOnboardingWrapper() }
+            Button("Onboard Device") { presented = true }
+            Spacer()
+        }
+    }
+}
+
+struct DeviceOnboardingWrapper: UIViewControllerRepresentable {
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+    }
+
+    func makeUIViewController(context: Context) -> UIViewController {
+        return DeviceManagerVC()
+    }
+}
+
+
+
+// MARK: Device VC
+
+class DeviceManagerVC: UINavigationController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let vc = UIHostingController(rootView: FirstDeviceScreen())
+        pushViewController(vc, animated: false)
     }
+}
 
-
+struct FirstDeviceScreen: View {
+    var body: some View {
+        VStack {
+            Text("First Device Screen")
+            Spacer()
+        }
+        .navigationBarHidden(true)
+    }
 }
 
